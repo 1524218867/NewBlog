f7c494ecee8235113f80d414409b2c238385a2a2
c x d <cxd@cdeMacBook-Air.local>
2024-11-24 16:28:31 +0800

    tijiao

--- vue.config.js
+++ vue.config.js
@@ -14,17 +14,17 @@ module.exports = defineConfig({
 
   lintOnSave: false, // 关闭 ESLint
 
-  devServer: {
-    proxy: {
-      '/uploads': {
-        target: 'http://localhost:3000',//处理前端8080后端5000跨域问题
-        changeOrigin: true,
-      },
-      '/api': {
-        target: 'http://localhost:3000', // 代理 /api 请求到后端
-        changeOrigin: true,
-        // pathRewrite: { '^/api': '' }, // 如果后端没有 `/api` 前缀，可以进行路径重写
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/uploads': {
+  //       target: 'http://localhost:3000',//处理前端8080后端5000跨域问题
+  //       changeOrigin: true,
+  //     },
+  //     '/api': {
+  //       target: 'http://localhost:3000', // 代理 /api 请求到后端
+  //       changeOrigin: true,
+  //       // pathRewrite: { '^/api': '' }, // 如果后端没有 `/api` 前缀，可以进行路径重写
+  //     },
+  //   },
+  // },
 });
